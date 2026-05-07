3da796bd79302c6569c3dfbd019be8ee3135f97f
HCANERE <hcanere@gmail.com>
2026-05-07 23:29:37 +0300

    Binary çalıştı

--- test.py
+++ test.py
@@ -3,9 +3,12 @@ from Image_Capture import load_image, show_image
 
 test=load_image(r"C:\Users\ASUS\Desktop\ATAKAN.jpeg")
 
-gray_img=ImageProcessor.turn_gray(test)
+#gray_img=ImageProcessor.turn_gray(test)
 
-show_image(gray_img,title="gri dönüşüm")
+#show_image(gray_img,title="gri dönüşüm")
+
+binary_image=ImageProcessor.turn_binary(test)
+show_image(binary_image,title="Binary daaa")
 
 
 
