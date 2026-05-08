d38ebf28617f21717dcaf5701b89826ce7892846
HCANERE <hcanere@gmail.com>
2026-05-08 19:58:12 +0300

    Caner BABA TÜM FONKSİYONLARI BİTİRDİ PULLA PULLLA KULLAN

--- Processor.py
+++ Processor.py
@@ -30,4 +30,36 @@ class ImageProcessor:
         return binary    
 
 
-        
+    #Arayüze not : Ömer kanka bu her çağrıldığında 90 derece dönüyor gösterebilir bunu sunumda
+    @staticmethod
+    def rotation_image(image):
+        if image.ndim==3: 
+            transposed=image.transpose(1,0,2) #Transpose alarak sadece eksenlerin yerlerini değiştiriyoruz
+
+        else : transposed=image.T #Binary ve gray için transpose
+
+        rotated=transposed[:,::-1, ...] #YÜKSEKLİK :OLDUĞU GİBİ , GENİŞLİK: terse çevir  , Kanal : oldıuğu gibi kalsın
+        return rotated
+    
+    @staticmethod
+    def crop_image(image,x,y,width,height):
+        result_image=image.copy()
+
+        max_crop_x=result_image.shape[1] #Maks kırıpılacak genişlik
+        max_crop_y=result_image.shape[0] #Maks yükseklik
+
+        if x+width<=max_crop_x and y+height<=max_crop_y: #yükseklik ve genişlik aşıldı mı kontrol
+            
+            if image.ndim==3: # Boyut kontrol
+
+                result_image[y:y+height,x:x+width,:]=0
+
+            else:    
+                result_image[y:y+height,x:x+width,]=0
+
+        return result_image
+
+
+
+    
+
