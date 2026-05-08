ad8722879d059b6dbce1d7c8ea2e45a112b27edd
HCANERE <hcanere@gmail.com>
2026-05-08 19:56:10 +0300

    Tüm çıktılar adam akıllı alındı

--- test.py
+++ test.py
@@ -7,8 +7,18 @@ test=load_image(r"C:\Users\ASUS\Desktop\ATAKAN.jpeg")
 
 #show_image(gray_img,title="gri dönüşüm")
 
-binary_image=ImageProcessor.turn_binary(test)
-show_image(binary_image,title="Binary daaa")
+# binary_image=ImageProcessor.turn_binary(test)
+# show_image(binary_image,title="Binary daaa")
+
+# rotation_image=ImageProcessor.rotation_image(test)
+# rotation_image=ImageProcessor.rotation_image(rotation_image)
+# rotation_image=ImageProcessor.rotation_image(rotation_image)
+# rotation_image=ImageProcessor.rotation_image(rotation_image)
+
+# show_image(rotation_image,title="90 DERECE DÖNMÜŞ")
+
+cropped_image=ImageProcessor.crop_image(test,100,150,100,50)
+show_image(cropped_image,title="kırpma")
 
 
 
